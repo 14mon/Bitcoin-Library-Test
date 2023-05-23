25226f5791caaa82604cb419a309d7efc442750e
eimyatmyatmon <eimyatmyatmon@abccsmm.com>
2023-05-23 11:24:54 +0630

    Generate Public Key

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NBitcoin;
+
+namespace _125350929
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Key privateKey = new Key(); // generate a random private key
+            PubKey publicKey = privateKey.PubKey;
+            Console.WriteLine("Key Generate " + publicKey); // 0251036303164f6c458e9f7abecb4e55e5ce9ec2b2f1d06d633c9653a07976560c
+
+        }
+    }
+}
