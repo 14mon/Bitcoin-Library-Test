1a799bed609dc14e6c5673d08a596ed1116879d3
eimyatmyatmon <eimyatmyatmon@abccsmm.com>
2023-05-23 12:19:14 +0630

    Generate Public Key with Hash and Bitcoin Address2

--- Program.cs
+++ Program.cs
@@ -32,6 +32,38 @@ namespace _125350929
             Console.WriteLine("TestNet with publicKeyHash:: " + testNetAddress);
             Console.WriteLine();
 
+            Console.WriteLine("We are able to generate the ScriptPubKey from the Bitcoin Address. This is a step that all bitcoin clients do to translate the “human friendly” Bitcoin Address to the Blockchain readable address.");
+
+            var publicKeyHash1 = new KeyId("14836dbe7f38c5ac3d49e8d790af808a4ee9e323");
+
+            var testNetAddress1 = publicKeyHash1.GetAddress(Network.TestNet);
+            var mainNetAddress1 = publicKeyHash1.GetAddress(Network.Main);
+
+            Console.WriteLine(mainNetAddress1.ScriptPubKey);
+            Console.WriteLine(testNetAddress1.ScriptPubKey);
+            Console.WriteLine();
+
+            Console.WriteLine("go backwards and generate a bitcoin address from the ScriptPubKey and the network identifier");
+            var paymentScript = publicKeyHash.ScriptPubKey;
+            var sameMainNetAddress = paymentScript.GetDestinationAddress(Network.Main);
+            var sameMainNetAddress2 = paymentScript.GetDestinationAddress(Network.TestNet);
+
+            Console.WriteLine("sameMainNetAddress From Main::" + sameMainNetAddress);
+            Console.WriteLine("MainNetAddress From Main:: " + mainNetAddress);
+            Console.WriteLine(mainNetAddress == sameMainNetAddress); // True
+
+            Console.WriteLine();
+
+            Console.WriteLine("sameMainNetAddress From TestNet:: " + sameMainNetAddress2);
+            Console.WriteLine("MainNetAddress From TestNet:: " + testNetAddress);
+            Console.WriteLine(testNetAddress == sameMainNetAddress2);
+
+            Console.WriteLine();
+            Console.WriteLine("retrieve the hash from the ScriptPubKey and generate a Bitcoin Address");
+            var samePublicKeyHash = (KeyId)paymentScript.GetDestination()!;
+            Console.WriteLine(publicKeyHash == samePublicKeyHash); // True
+            var sameMainNetAddress3 = new BitcoinPubKeyAddress(samePublicKeyHash, Network.Main);
+            Console.WriteLine(mainNetAddress == sameMainNetAddress3); // True
         }
     }
 }
